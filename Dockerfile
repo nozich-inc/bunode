88cb3189b410bf4189d75a617801734f450602c3
Anilcan Kara <anilcankara.com@gmail.com>
2024-03-17 03:39:03 +0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM oven/bun:latest
+
+SHELL ["/bin/bash", "--login", "-c"]
+
+RUN apt-get update && apt-get install -y curl zsh vim docker.io unzip git python3 python3-pip
+RUN bun upgrade
+
+RUN curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | zsh || true
+RUN curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+RUN nvm install --lts
+RUN nvm use --lts
+RUN npm install -g npm@latest
+
+RUN pip3 install --upgrade pip
+RUN pip3 install --upgrade awscli
+
+RUN echo "alias python=$(which python3)" >> ~/.bashrc
+RUN echo "alias pip=$(which pip3)" >> ~/.bashrc
+
+RUN source ~/.bashrc
+
+RUN git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+RUN sh ~/.vim_runtime/install_awesome_vimrc.sh
+RUN zsh -c "cd ~/.vim_runtime && git reset --hard && git clean -d --force && git pull --rebase && python3 update_plugins.py"
+
+RUN echo "source ~/.bashrc" >> ~/.zshrc
+
+WORKDIR /app
---
+++ push.js
@@ -0,0 +1,70 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+const $ = (cmd) => exec(cmd)
+  .then(({ stdout, stderr, code }) => ({ stdout: stdout.trim(), stderr, code }))
+  .catch((e) => e);
+
+const img = 'nozich/bunode'
+
+async function main() {
+  await $(`docker pull ${img}:latest`)
+
+  const { stdout: inspect } = await $(`docker buildx inspect | grep 'Platforms:' | sed 's/Platforms: //'`);
+
+  const platforms = inspect.split(',').map(e => e.trim()).map((p) => ({
+    platform: p,
+    os: p.split('/')[0],
+    arch: p.split('/')[1],
+    tag: p.replace(/[^a-zA-Z0-9]/g, '-').toLowerCase()
+  }));
+
+  console.log(`platforms`, platforms.map((p) => p.tag).join(','));
+
+  const built = (await Promise.all(platforms.map(async ({ platform, tag }) => {
+    const build = await $(`docker buildx build --platform=${platform} -t ${img}:${tag} --push .`)
+      .catch((e) => e);
+
+    console.log(`[BUILD] [END] [${platform}] [${tag}] {code: ${build.code}}`);
+
+    return (!build.code || build.code === 0) && tag
+  }))).filter(Boolean);
+
+  console.log(`built`, built);
+
+  if (built.length === 0) {
+    console.error('No images were built');
+    return;
+  }
+
+  const currents = await fetch(`https://registry.hub.docker.com/v2/repositories/${img}/tags/`)
+    .then((res) => res.json())
+    .then((data) => data.results
+      .find((r) => r.name === 'latest')
+      .images.map((i) => ({
+        ...i,
+        arch: i.architecture,
+        tag: i.variant ? `${i.os}-${i.architecture}-${i.variant}` : `${i.os}-${i.architecture}`
+      })));
+
+  const annotates = currents || []
+
+  const news = platforms.filter((p) => built.includes(p.tag))
+
+  news.forEach((p) => {
+    if (annotates.find((a) => a.tag === p.tag)) return;
+    annotates.push(p);
+  });
+
+  console.log(`annotates`, annotates.map(({ tag, os, arch }) => ({ tag, os, arch })));
+
+  await $(`docker manifest create --amend ${img}:latest ${annotates.map(({ tag }) => `${img}:${tag}`).join(' ')}`);
+
+  await Promise.all(annotates.map(async ({ os, arch, tag }) => {
+    return $(`docker manifest annotate ${img} ${img}:${tag} --os ${os} --arch ${arch}`);
+  }));
+
+  await $(`docker manifest push --purge ${img}:latest`)
+}
+
+main();
